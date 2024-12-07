3218e7fc74b4749ee197a0daf798e627261f6414
ayman nabgouri <nabgouri9@gmail.com>
2024-12-07 11:23:45 +0100

    changing the supabse env key

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import ProjectCard from "../components/ProjectCard";
-const SUPABASE_URL = "https://cujdcokjjculjibmtjnj.supabase.co";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL_KEY;
 const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 export default function PortfolioPage() {
   const [projects, setProjects] = useState([]);
